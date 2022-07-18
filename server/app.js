ad5b2b314aed67ce32a0fa705a3a095b63fb1902
Justin Mathew <justinmathew@Justins-Air.fritz.box>
2022-07-18 20:01:21 +0200

    Adding mongo db database and relationships with books and authors

--- server/app.js
+++ server/app.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema')
-
+const mongoose = require('mongoose');
 const app = express();
+
+//const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://mongouser:test123@cluster0.8rdif.mongodb.net/?retryWrites=true&w=majority";
+/*
+const client = new MongoClient(uri, 
+    { useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    serverApi: ServerApiVersion.v1 }
+    );
+
+client.connect();
+client.once('open',()=>{
+    console.log("connected");
+})
+*/
+mongoose.connect(uri);
+mongoose.connection.once('open',()=>{
+    console.log('connected');
+});
+
 app.use('/graphql', graphqlHTTP({
 schema,
 graphiql: true
